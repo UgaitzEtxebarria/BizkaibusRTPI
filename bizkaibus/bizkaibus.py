0178d15733734465643b437541c296b71e0ab221
UgaitzEtxebarria <etxeba17@gmail.com>
2025-07-12 01:11:14 +0200

    Naming change

--- bizkaibus/bizkaibus.py
+++ bizkaibus/bizkaibus.py
@@ -60,18 +60,18 @@ from typing import Optional
 
 class BizkaibusArrival:
     line: BizkaibusLine
-    closestArrival: BizkaibusArrivalTime
-    farestArrival: Optional[BizkaibusArrivalTime] = None
+    nearestArrival: BizkaibusArrivalTime
+    nextArrival: Optional[BizkaibusArrivalTime] = None
 
-    def __init__(self, line: BizkaibusLine, closestArrival: BizkaibusArrivalTime, farestArrival: Optional[BizkaibusArrivalTime] = None):
+    def __init__(self, line: BizkaibusLine, nearestArrival: BizkaibusArrivalTime, nextArrival: Optional[BizkaibusArrivalTime] = None):
         """Initialize the data object."""
         self.line = line
-        self.closestArrival = closestArrival
-        self.farestArrival = farestArrival
+        self.nearestArrival = nearestArrival
+        self.nextArrival = nextArrival
     
     def __str__(self):
         """Return a string representation of the object."""
-        return f"Line: {self.line}, closest: {self.closestArrival}, farest: {self.farestArrival}"
+        return f"Line: {self.line}, nearest: {self.nearestArrival}, next: {self.nextArrival}"
 
 class BizkaibusTimetable:
     """The class for handling the data retrieval."""
